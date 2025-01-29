cb7cbcea2843a777900cdbbdae304c0cc5a1c4f7
Peter Radocchia <radocchia@gmail.com>
2025-01-29 17:25:09 -0500

    tuning road running

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/drive/DriveConstants.java
@@ -56,8 +56,8 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 1.0 / rpmToVelocity(MAX_RPM);
-    public static double kA = 0;
+    public static double kV = 0.02205718327157626;
+    public static double kA = 0.004;
     public static double kStatic = 0;
 
     /*
@@ -88,10 +88,10 @@ public class DriveConstants {
      * You are free to raise this on your own if you would like. It is best determined through experimentation.
      
      */
-    public static double MAX_VEL = 41.72427743048944;
-    public static double MAX_ACCEL = 41.72427743048944;
-    public static double MAX_ANG_VEL = Math.toRadians(183.89423076923075);
-    public static double MAX_ANG_ACCEL = Math.toRadians(183.89423076923075);
+    public static double MAX_VEL = 35;
+    public static double MAX_ACCEL = 35;//41.72427743048944;
+    public static double MAX_ANG_VEL = Math.toRadians(161.273);
+    public static double MAX_ANG_ACCEL = Math.toRadians(161.273);// Math.toRadians(183.89423076923075);
 
 
     public static double encoderTicksToInches(double ticks) {
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/drive/StandardTrackingWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/drive/StandardTrackingWheelLocalizer.java
@@ -31,7 +31,7 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
     public static double WHEEL_RADIUS = 0.6889764; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (encoder) speed
 
-    public static double LATERAL_DISTANCE = 13.28; // in; distance between the left and right wheels (13.38)
+    public static double LATERAL_DISTANCE = 13.466; //fast 13.28// in; distance between the left and right wheels (13.38)
     public static double FORWARD_OFFSET = 1.625; // in; offset of the lateral wheel
 
 
